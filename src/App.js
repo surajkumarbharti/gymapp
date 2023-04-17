13c87fa8daef5efdb81532d5ddf613c39b067a22
surajkumarbharti <103020511+surajkumarbharti@users.noreply.github.com>
2023-04-17 16:30:05 +0530

    finished

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Navbar from './Components/Navbar';
+import Header from './Components/Header';
+import Feature from './Components/Feature';
+import Offer from './Components/Offer';
+import About from './Components/About';
+import Contact from './Components/Contact';
+import Footer from './Components/Footer'
+import Admin from './Components/Admin'
+
+
+function App() {
+  return (
+    
+    <div className="App">
+     <Navbar/>
+     <Header/>
+     <Feature/>
+     <Offer/>
+     <Admin/>
+     <About/>
+     <Contact/>
+     <Footer/>
+    </div> 
+  );
+}
+
+export default App;
---
+++ src/Components/Admin.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function admin() {
+  return (
+    <div id = 'about'>
+        <div className='about'>   
+        </div>
+            <h1>Admin Page</h1>
+            <p>Check All User</p>
+                  <button>Check MORE</button>
+    </div>
+  )
+}
+
+export default admin
